565b07a5957074f71ba30f665529b0ffb370c001
cyan <2912161834@qq.com>
2024-10-08 09:42:46 +0800

    前端工程化学习文档仓库初始化

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+export default defineConfig({
+    title: "Vue 3 + TypeScript 学习文档",
+    description: "详细学习Vue 3 和 TypeScript 的指南",
+    themeConfig: {
+        siteTitle: "前端学习",
+        logo: "/assets/logo.png",
+        nav: [
+            {text: "首页", link: "/"},
+            {text: "指南", link: "/guide/"},
+            {text: "组件", link: "/components/"},
+            {text: "API 参考", link: "/api/"},
+            {text: "常见问题", link: "/faq/"},
+        ],
+        socialLinks: [
+            {icon: "github", link: "https://github.com/vuejs/vitepress"},
+        ],
+        sidebar: {
+            "/guide/":[
+                {
+                    text: "开始",
+                    collapsible: true,
+                    items: [
+                        {text: "介绍", link: "/guide"},
+                        {text: "安装", link: "/guide/installation"},
+                        {text: "基本概念", link: "/guide/concepts"},
+                    ],
+                },
+            ],
+        },
+        footer: {
+            message: "用心学习 Vue 3 和 TypeScript",
+            copyright: "Copyright @ 2024 cyan",
+        },
+    },
+});
+
---
+++ docs/index.md
@@ -0,0 +1,35 @@
+---
+layout: home
+
+hero:
+  name: "前端工程化"
+  text: "Vue3 + TypeScript 学习文档"
+  tagline: "学习是少数人的狂欢，亦是孤独者的游戏"
+  image:
+    src: /assets/logo.png
+    alt: Chrom 浏览器
+  actions:
+    - theme: brand
+      text: 快速开始
+      link: /guide/
+    - theme: alt
+      text: 常见问题
+      link: /faq/
+          
+features:
+  - icon: 💥
+    title: 轻松入门
+    details: 从头开始，循序渐进地学习 Vue 3 和 TypeScript
+  - icon: 💻
+    title: 最佳实践
+    details: 涵盖从组件设计到代码优化的实际开发经验
+  - icon: 🚀
+    title: 高效开发
+    details: 通过 VitePress 和 TypeScript, 快速搭建高效、可维护的 Vue 3 应用
+---
+
+
+<div style="text-align: center; margin-top: 50px;">
+  <em>💫 Happy Coding 💫</em>
+</div>
+         
